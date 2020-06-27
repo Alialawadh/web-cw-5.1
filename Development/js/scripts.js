38dbfd2003c5b014cd46151b510c417fbe032d1b
Alialawadh <ali92002@htomail.com>
2020-06-27 20:49:31 +0300

    خلصت

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,54 @@
 // السؤال الأول
 // 1
-
+let name = ("علي");
 // 2
-
+let age = ("17");
 // 3 and 4
-
+console.log("انا" + " " + name + " " + "وعمري" + age );
+     
 // 5 and 6
+age *= 5;
+console.log(age*5);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+const hobbies=["swimming" ,"vedio games","reading"];
+    
 
 // 2
-
+hobbies.push(17);
+console.log(hobbies);
+hobbies.push=("الويب,HTML");
+console.log(hobbies);
 // 3
-
+hobbies.pop("reading");
+console.log(hobbies);
 // 4
-
+let properties = {
+    name: "web",
+    language: "html",
+    ta:[
+         "hussain",
+         "moddi",
+         "nansy",
+         " Rahaf"
+    ]
+};
 // 5
 
 // 6
-
+console.log(properties.name);
 // 7
 
+console.log(properties.ta[0]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
 
+}
 // 2
 
 // console results => console.png
